7a04c102d424da2aacb8b600f9de97e07c10b41e
Mihkel Küla <mihkel.kula@tktk.ee>
2024-01-06 20:58:00 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine());
+
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
